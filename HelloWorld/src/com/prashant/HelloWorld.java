fa375223a9dd52b725584f62f4ce69fd92203d40
prasuprety <prasuprety@gmail.com>
2017-09-07 15:29:06 -0700

    Added ! in the message.

--- HelloWorld/src/com/prashant/HelloWorld.java
+++ HelloWorld/src/com/prashant/HelloWorld.java
@@ -3,7 +3,7 @@ package com.prashant;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World! Welcome to GitHub");
+		System.out.println("Hello World! Welcome to GitHub!");
 	}
 
 }
